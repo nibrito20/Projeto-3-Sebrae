731a17207ca55f10ef4594d1a5364a6e3ad0546f
nibrito20 <minorbrito20@gmail.com>
2026-05-07 09:02:23 -0300

    permitindo CORS para localhost e deploy oficial

--- SEBRAEProjeto/src/main/java/br/edu/cs/projetos3/sebrae/feedback/CorsConfig.java
+++ SEBRAEProjeto/src/main/java/br/edu/cs/projetos3/sebrae/feedback/CorsConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${frontend.url:http://localhost:5173}")
-    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(frontendUrl)
-                .allowedMethods("GET", "POST", "OPTIONS")
-                .allowedHeaders("*");
+
+                .allowedOrigins("http://localhost:5173", "https://lens-sebrae.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
